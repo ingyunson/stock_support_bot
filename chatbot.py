569c9268ef13c16efb59f2ea1c381466a55acedb
Ingyun Son <ingyunson@gmail.com>
2018-12-18 23:38:29 +0900

    Update: make function to send all users in the user db

--- chatbot.py
+++ chatbot.py
@@ -6,12 +6,19 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 now = datetime.datetime.now()
 nowDate = now.strftime('%Y-%m-%d')
 
-today_finance = pd.read_csv('{}_KRX_finance.csv'.format(nowDate))
-today_stock = pd.read_csv('{}_KRX_stock.csv'.format(nowDate))
+my_token = '729504243:AAFQEqyGx_yjOkSEBoNCUToP2KLH0VR-WX4'
+bot = telegram.Bot(token = my_token)   #bot을 선언합니다.
+
+updater = Updater(my_token)
+updates = bot.getUpdates()  #업데이트 내역을 받아옵니다.
+
+today_finance = pd.read_csv('KRX_finance.csv'.format(nowDate))
+today_stock = pd.read_csv('KRX_stock.csv'.format(nowDate))
 
 finance_condition = [100, 100, 100] #자본유보율, 연매출, 부채비율 조건 조정
 stock_condition = [10, 10, 10, 10, 10, 10, 10, 10] #PER, PBR, ROIC, ROE, BPS, EPS, 수익율 조절 가능
-
+user = pd.DataFrame(columns = ['User_id', '자본유보율', '연매출', '부채비율', 'PER', 'PBR', 'ROIC', 'ROE', 'BPS', 'EPS', '수익률'])
+userdata = pd.read_csv('test.csv')
 
 today_stock['BPS_mul'] = today_stock['BPS'] * stock_condition[4]
 today_stock['EPS_mul'] = today_stock['EPS'] * stock_condition[5]
@@ -25,17 +32,6 @@ today_stock[(today_stock['PER'] < stock_condition[0]) & (today_stock['PBR'] < st
 
 
 # 챗봇 part
-
-my_token = <Telegram Token>
-bot = telegram.Bot(token=my_token)  # bot을 선언합니다.
-
-updater = Updater(my_token)
-updates = bot.getUpdates()  # 업데이트 내역을 받아옵니다.
-user_id = bot.getUpdates()[-1].message.chat.id
-user_info = [user_id] + finance_condition + stock_condition
-user_data = [user_info]
-df_user = pd.DataFrame(user_data, columns = ['User_id', '자본유보율', '연매출', '부채비율', 'PER', 'PBR', 'ROIC', 'ROE', 'BPS', 'EPS', '수익률'])
-
 
 # 자본유보율, 연매출, 부채비율 조건 조정
 # PER, PBR, ROIC, ROE, BPS, EPS, 수익율
@@ -56,11 +52,17 @@ def join_command(bot, update):
             id=user_id, cap=user_info[1], ben=user_info[2], debt=user_info[3], per=user_info[4], pbr=user_info[5],
             roic=user_info[6], roe=user_info[7], bps=user_info[8], eps=user_info[9], earn=user_info[10]))
 
+
+
+
+for id in userdata['User_id']:
+    print(id)
+
 print('chatbot is ready')
 start_handler = CommandHandler('start', start_command)
 join_handler = CommandHandler('join', join_command)
 updater.dispatcher.add_handler(start_handler)
 updater.dispatcher.add_handler(join_handler)
 
-updater.start_polling(timeout=1, clean=True)
+updater.start_polling()
 updater.idle()
