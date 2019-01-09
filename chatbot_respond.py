00ce06c552ceec87137bd9d812cc014cfe7f85c1
Ingyun Son <ingyunson@gmail.com>
2019-01-10 00:45:58 +0900

    Update: add callback

--- chatbot_respond.py
+++ chatbot_respond.py
@@ -31,15 +31,26 @@ def start_command(bot, update):
 
 
 def config(bot, update):
-    keyboard = [[InlineKeyboardButton("자본유보율", callback_data="cap"), InlineKeyboardButton("연매출", callback_data="ben"), InlineKeyboardButton("부채비율", callback_data="debt")],
-                [InlineKeyboardButton("PER", callback_data="per"), InlineKeyboardButton("PBR", callback_data="pbr"), InlineKeyboardButton("ROIC", callback_data="roic")],
-                [InlineKeyboardButton("ROE", callback_data="roe"), InlineKeyboardButton("EPS", callback_data="eps")],
-                [InlineKeyboardButton("매수적정가", callback_data="buy"), InlineKeyboardButton("매도적정가", callback_data="sell")],
+    keyboard = [[InlineKeyboardButton("자본유보율", callback_data="자본유보율"), InlineKeyboardButton("연매출", callback_data="연매출"), InlineKeyboardButton("부채비율", callback_data="부채비율")],
+                [InlineKeyboardButton("PER", callback_data="PER"), InlineKeyboardButton("PBR", callback_data="PBR"), InlineKeyboardButton("ROIC", callback_data="ROIC")],
+                [InlineKeyboardButton("ROE", callback_data="ROE"), InlineKeyboardButton("EPS", callback_data="EPS")],
+                [InlineKeyboardButton("매수적정가", callback_data="매수적정가"), InlineKeyboardButton("매도적정가", callback_data="매도적정가")],
                 [InlineKeyboardButton("cancel", callback_data="cancel")]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
+def callback_set(bot, update):
+    print("callback")
+    cb = update.callback_query.data
+    if cb in userdata.columns:
+        bot.edit_message_text(text="{select}이(가) 선택되었습니다. {select}의 현재 설정값은 {num}입니다.".format(select=cb, num=100),
+                              chat_id=update.callback_query.message.chat_id,
+                              message_id=update.callback_query.message.message_id)
+    else:
+        bot.edit_message_text(text="잘못된 입력입니다.", chat_id=update.callback_query.message.chat_id,
+                              message_id=update.callback_query.message.message_id)
 
 
 def message(bot, update):
@@ -74,9 +85,10 @@ def main():
 
     print('chatbot is ready')
     start_handler = CommandHandler('start', start_command)
-    config_handler = CommandHandler('config', config)
+    config_handler = CommandHandler('set', set)
     dp.add_handler(config_handler)
     dp.add_handler(start_handler)
+    dp.add_handler(CallbackQueryHandler(callback_set))
 
     updater.start_polling()
     updater.idle()
